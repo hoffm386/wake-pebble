dfb6dbd160641bcb4e0eaa1d03014f9f11c1e5c1
Erin Hoffman <hoffm386@msu.edu>
2015-02-21 09:08:58 -0800

    Initial commit of Pebble code
    Right now it queries the Ruby on Rails endpoint every 10 seconds asking if the user is asleep.  It's supposed to be displaying the sleep state (0 or 1) on the screen but it's showing 5369958 right now

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,69 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  // Construct URL
+  var url = "http://wake-treehacks.herokuapp.com/asleep";
+
+  // Send request to Wake backend
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+
+      var asleep = json.SLEEP;
+      console.log("Asleep value is " + asleep);
+      
+      // Assemble dictionary using our keys
+      var dictionary = {
+        "KEY_ASLEEP": asleep
+      };
+
+      // Send to Pebble
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log("Weather info sent to Pebble successfully!");
+        },
+        function(e) {
+          console.log("Error sending weather info to Pebble!");
+        }
+      );
+    }      
+  );
+}
+
+function locationError(err) {
+  console.log("Error requesting location!");
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maximumAge: 60000}
+  );
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+
+    // Get the initial weather
+    getWeather();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log("AppMessage received!");
+    getWeather();
+  }                     
+);
