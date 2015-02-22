418161f203e60fb6c75f7545b65586be6f7df741
Erin Hoffman <hoffm386@msu.edu>
2015-02-21 18:41:42 -0800

    No clock text and image changes based on state
    If not sleeping, image is logo.png.  If sleeping, image is wake_up.png

--- src/main.c
+++ src/main.c
@@ -10,76 +10,35 @@ static TextLayer *s_time_layer;
 static GFont s_time_font;
 
 static BitmapLayer *s_background_layer;
-static GBitmap *s_background_bitmap;
+static GBitmap *s_background_bitmap_logo;
+static GBitmap *s_background_bitmap_wake_up;
 
 int num_queries = 0;
 
-static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-
-  // Create a long-lived buffer
-  static char buffer[] = "00:00";
-
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    //Use 2h hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
-    //Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-  }
-
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
-}
-
 static void main_window_load(Window *window) {
-  //Create GBitmap, then set to created BitmapLayer
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  //Create GBitmaps, then set logo one to created BitmapLayer
+  s_background_bitmap_logo = gbitmap_create_with_resource(RESOURCE_ID_LOGO_IMAGE);
+  s_background_bitmap_wake_up = gbitmap_create_with_resource(RESOURCE_ID_WAKE_UP_IMAGE);
+  
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap_logo);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
-  // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
-  
-  //Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
-
-  //Apply to TextLayer
-  text_layer_set_font(s_time_layer, s_time_font);
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Number of total queries: %d", num_queries);
-  
-  // Make sure the time is displayed from the start
-  update_time();
 }
 
 static void main_window_unload(Window *window) {
-  //Unload GFont
-  fonts_unload_custom_font(s_time_font);
   
-  //Destroy GBitmap
-  gbitmap_destroy(s_background_bitmap);
+  //Destroy GBitmaps
+  gbitmap_destroy(s_background_bitmap_logo);
+  gbitmap_destroy(s_background_bitmap_wake_up);
 
   //Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
   
-  // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
   
   // Get sleep status update every 10 seconds
   if(tick_time->tm_sec % 10 == 0) {
@@ -113,7 +72,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
       int asleep = (int8_t)t->value->int8;
       if (asleep) {
         APP_LOG(APP_LOG_LEVEL_INFO, "Asleep was true");
-        
+        bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap_wake_up);
         // Vibe pattern: ON for 1000ms, OFF for 800ms, ON for 1000ms:
         static uint32_t const segments[] = { 1000, 800, 1000, 800, 1000, 800, 1000 };
         VibePattern pat = {
@@ -123,6 +82,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
         vibes_enqueue_custom_pattern(pat);
       } else {
         APP_LOG(APP_LOG_LEVEL_INFO, "Asleep was false");
+        bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap_logo);
       }
       
       break;
